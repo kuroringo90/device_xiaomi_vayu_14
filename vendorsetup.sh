f2fbb21a4d85dee32b7ed421b41be70d2e37cb22
GXC2356 <gxcdev2356@gmail.com>
2024-08-02 16:15:45 +0000

    vayu: vendorsetup.sh: Clone hardware_xiaomi repo from LineageOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,3 +13,7 @@ git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
 # Dolby
 rm -rf vendor/dolby
 git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
+
+# Hardware
+rm -rf hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
