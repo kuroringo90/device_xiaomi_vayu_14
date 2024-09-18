1274823bfde3f16c963760cea01ebfbc0f058a1a
GXC2356 <108477326+GXC2356@users.noreply.github.com>
2024-09-18 22:46:55 +0300

    vendorsetup.sh: Remove Dolby and Gcam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,14 +6,6 @@ git clone --depth=1 https://github.com/SkylineUI-Devices/vendor_xiaomi_vayu.git 
 rm -rf kernel/xiaomi/vayu
 git clone --depth=1 https://github.com/GXC2356/platform_kernel_xiaomi_vayu.git kernel/xiaomi/vayu
 
-# Gcam
-rm -rf vendor/mgc
-git clone https://bitbucket.org/vendor-mgc/vendor_mgc.git vendor/mgc
-
-# Dolby
-rm -rf vendor/dolby
-git clone https://gitlab.com/dogpoopy/vendor_dolby.git vendor/dolby
-
 # Hardware
 rm -rf hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
